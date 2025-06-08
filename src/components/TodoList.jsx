1ce0895905360d5c5a33b4bbd359eab1cfe4caae
priyalhotmail <priyal_hdp@hotmail.com>
2025-06-08 17:15:10 +0200

    update css of form and todo List

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,12 @@
 import TodoItem from "./TodoItem";
+import styles from "./todolist.module.css";
 
 export default function TodoList({ todos, setTodos }) {
     const sortedTodos = todos
         .slice()
         .sort((a, b) => Number(a.done) - Number(b.done));
     return (
-        <div>
+        <div className={styles.list}>
             {sortedTodos.map((item, index) => (
                 <TodoItem
                     item={item}
